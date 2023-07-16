7439dc0abaa2acc9126fcb904a2ea210edff4b97
ibrahima diagne seck <ibrahimadiagneseck@esp.sn>
2023-07-16 23:48:26 +0000

    commit

---
+++ REACTJS/mon-projet/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import FizzBuzz from './FizzBuzz';
+
+function App() {
+  return (
+    <div>
+      <h1>FizzBuzz</h1>
+      <FizzBuzz />
+    </div>
+  );
+}
+
+export default App;
---
+++ REACTJS/mon-projet/src/FizzBuzz.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+function FizzBuzz() {
+    
+    const resultats = [];
+
+    for (let i = 1; i <= 100; i++) {
+        let chaine = '';
+
+        if (i % 3 === 0) {
+            chaine += 'Fizz';
+        }
+        if (i % 5 === 0) {
+            chaine += 'Buzz';
+        }
+
+        if (chaine === '') {
+            resultats.push(i);
+        } else {
+            resultats.push(chaine);
+        }
+    }
+
+    return (
+        <div>
+            <ul>
+                {resultats.map((resultat, index) => (
+                    <li key={index}>{resultat}</li>
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+export default FizzBuzz;
