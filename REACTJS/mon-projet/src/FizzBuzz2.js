5f6ecb4dfaac5e9ec752f5c08e1591012148d28e
ibrahima diagne seck <ibrahimadiagneseck@esp.sn>
2023-07-17 00:22:21 +0000

    commit

--- REACTJS/mon-projet/src/FizzBuzz2.js
+++ REACTJS/mon-projet/src/FizzBuzz2.js
@@ -1,35 +1,45 @@
 import React from 'react';
 
 function FizzBuzz2() {
-    
-    const resultats = [];
+  const resultats = [];
 
-    for (let i = 1; i <= 100; i++) {
-        let chaine = '';
+  for (let i = 1; i <= 100; i++) {
+    let chaine = '';
 
-        if (i % 3 === 0) {
-            chaine += 'Fizz';
-        }
-        if (i % 5 === 0) {
-            chaine += 'Buzz';
-        }
+    const divisible3 = i % 3 === 0;
+    const divisible5 = i % 5 === 0;
+    const contient3 = String(i).includes('3');
+    const contient5 = String(i).includes('5');
 
-        if (chaine === '') {
-            resultats.push(i);
-        } else {
-            resultats.push(chaine);
-        }
+    if (contient3) {
+      chaine += 'Fizz';
+    }
+    if (contient5) {
+      chaine += 'Buzz';
+    }
+    if (divisible3) {
+      chaine += 'Fizz';
+    }
+    if (divisible5) {
+      chaine += 'Buzz';
     }
 
-    return (
-        <div>
-            <ul>
-                {resultats.map((resultat, index) => (
-                    <li key={index}>{resultat}</li>
-                ))}
-            </ul>
-        </div>
-    );
+    if (chaine === '') {
+      resultats.push(i);
+    } else {
+      resultats.push(chaine);
+    }
+  }
+
+  return (
+    <div>
+      <ul>
+        {resultats.map((resultat, index) => (
+          <li key={index}>{resultat}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default FizzBuzz2;
